c8b0849d5c69c3533493ae1b3e2a2d8482f9964d
sawayam-chaudhary <sawayamonly@gmail.com>
2026-02-21 00:31:36 +0530

    Implemented full job control support : job_table, fg, bg, cleanup_job, print_jobs

--- shell.c
+++ shell.c
@@ -5,8 +5,140 @@
 #include <unistd.h>
 #include <signal.h>
 #include <fcntl.h>
+#define max_jobs 100
+
+typedef enum job_state{
+    RUNNING,
+    STOPPED,
+    DONE
+}job_state;
+typedef struct job{
+    int job_id;
+    pid_t pgid;
+    char command[1024];
+    job_state state;
+}job;
+
+job jobs[max_jobs];
+int job_count=0;
+
+void add_job(pid_t pgid, char *cmd, job_state state){
+    if(job_count>=max_jobs){
+        printf("job table is full");
+        return;
+    }
+    jobs[job_count].job_id=job_count+1;
+    jobs[job_count].pgid = pgid;
+    strcpy(jobs[job_count].command,cmd);
+    jobs[job_count].state = state;
+    job_count++;
+}
+
+void print_jobs(){
+    for(int i=0;i<job_count;i++){
+        char *print_state;
+        if(jobs[i].state == RUNNING){
+            print_state = "RUNNING";
+        } else if(jobs[i].state== STOPPED){
+            print_state = "STOPPED";
+        } else {
+            print_state = "DONE";
+        }
+
+        printf("[%d] %s %s\n",jobs[i].job_id,print_state,jobs[i].command);
+}}
+
+
+void sigchld_handler(int sig){
+    int status;
+    pid_t pid;
+    while((pid=waitpid(-1,&status,WNOHANG | WUNTRACED | WCONTINUED))>0){
+
+            for(int i=0;i<job_count;i++){
+                if(pid==jobs[i].pgid){
+                    if(WIFEXITED(status) || WIFSIGNALED(status)){
+                        jobs[i].state=DONE;
+                    }
+                    else if(WIFSTOPPED(status)){
+                        jobs[i].state=STOPPED;  
+                    }
+                    else if(WIFCONTINUED(status)){
+                        jobs[i].state=RUNNING;
+                    }
+
+
+                
+            
+            break;
+                }
+
+            }}}
+
+void fg(int job_id, pid_t shell_pid){
+    if(job_id<=0 || job_id>job_count){
+        printf("invalid job\n");
+        return;
+    }
+    job *j = &jobs[job_id-1];
+    if(j->state==DONE){
+        printf("job already finished\n");
+        return;
+    }
+    pid_t pgid=j->pgid;
+    tcsetpgrp(STDIN_FILENO, pgid);
+    kill(-pgid, SIGCONT);
+    int status;
+    while(waitpid(-pgid, &status, WUNTRACED)>0);
+    tcsetpgrp(STDIN_FILENO, shell_pid);
+    if(WIFSTOPPED(status))
+        j->state = STOPPED;
+    else
+        j->state=DONE;
+}
+
+void bg(int job_id){
+    if(job_id<=0 || job_id>job_count){
+        printf("invalid job id\n");
+        return;
+    }
+    job *j = &jobs[job_id-1];
+    if(j->state==DONE){
+        printf("job is already completed");
+        return;
+    }
+    if(j->state==RUNNING){
+        printf("job is already running");
+        return;
+    }
+    pid_t pid = j->pgid;
+    kill(-pid,SIGCONT);
+    j->state=RUNNING;
+}
+
+
+void cleanup_jobs(){
+    for(int i=0; i<job_count; ){
+    if(jobs[i].state==DONE){
+    for(int j=i;j<job_count-1;j++){
+        
+        jobs[j]=jobs[j+1];
+        jobs[j].job_id=j+1;
+    }
+    job_count--;
+    }
+    else{
+        i++;
+    }
+    }
+}
+            
+            
+        
+
+
 
 int main(int argc, char *argv[]){
+    signal(SIGCHLD,sigchld_handler);
     signal(SIGINT, SIG_IGN);
     signal(SIGTSTP, SIG_IGN);
     pid_t shell_pid = getpid();
@@ -15,7 +147,7 @@ int main(int argc, char *argv[]){
 
     while(1){
         int status;
-        while(waitpid(-1,&status,WNOHANG)>0){};
+        
         printf("msh>");
         fflush(stdout);
 
@@ -67,6 +199,35 @@ int main(int argc, char *argv[]){
                 perror("pwd");
             continue;
         }
+
+        if(strcmp(myargv[0],"jobs")==0){
+                 print_jobs();
+                 cleanup_jobs();
+                 continue;
+
+                 }
+        if(strcmp(myargv[0],"fg")==0){
+            if(myargv[1]==NULL){
+                printf("job_id has not passed");
+            }else{
+                int job_id=atoi(myargv[1]);
+                fg(job_id,shell_pid);
+            }
+            continue;
+        }
+
+        if(strcmp(myargv[0],"bg")==0){
+            if(myargv[1]==NULL){
+                printf("job_id has not passed");
+            }else{
+                int job_id=atoi(myargv[1]);
+                bg(job_id);
+            }
+            continue;
+        }
+
+
+
 
         int background =0;
         if(i>0 && strcmp(myargv[i-1],"&")==0){
@@ -138,11 +299,15 @@ int main(int argc, char *argv[]){
                 close(pipes[j][0]);
                 close(pipes[j][1]);
                 }
-       if(!background)
+       if(background){
+           add_job(pgid,str,RUNNING);
+           printf("[%d] %d\n",job_count,pgid);
+       }
+       else{
         tcsetpgrp(STDIN_FILENO,pgid);
-        for(int j=0;j<cmd_count;j++){
-                wait(NULL);
-        }
+
+        int status;
+        while(waitpid(-pgid,&status,WUNTRACED)>0);
         tcsetpgrp(STDIN_FILENO,shell_pid);}
         continue;
         }
@@ -200,9 +365,14 @@ int main(int argc, char *argv[]){
             }
             close(fd);
             setpgid(pid,pid);
-            if(!background){
+            if(background){
+                add_job(pid,str,RUNNING);
+                printf("[%d] %d\n",job_count,pid);
+            }
+            else {
             tcsetpgrp(STDIN_FILENO,pid);
-            waitpid(pid,NULL,0);
+            int status;
+            waitpid(pid,&status,WUNTRACED);
             tcsetpgrp(STDIN_FILENO, shell_pid);
             }
             continue;
@@ -218,9 +388,15 @@ int main(int argc, char *argv[]){
             exit(1);
         }
         setpgid(pid,pid);
-        if(!background){
-        tcsetpgrp(STDIN_FILENO,pid);
-        waitpid(pid,NULL,0);
+        if(background){
+            add_job(pid,str,RUNNING);
+            printf("[%d] %d\n",job_count,pid);
+
+        }
+        else {
+    tcsetpgrp(STDIN_FILENO,pid);
+    int status;
+        waitpid(pid,&status,WUNTRACED);
         tcsetpgrp(STDIN_FILENO,shell_pid);
         }
     }
